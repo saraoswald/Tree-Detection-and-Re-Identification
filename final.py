17d2bed847f659433f98301e60895fe863cf499b
saraoswald <sara.linsley@gmail.com>
2016-05-08 21:27:34 -0400

    Tree struct
    also made some things functions

--- final.py
+++ final.py
@@ -3,6 +3,12 @@ import cv2
 from matplotlib import pyplot as plt
 from scipy.interpolate import spline
 
+class Tree:
+    def __init__(self, image, topLeft, bottomRight):
+        self.image = image
+        self.topLeft = topLeft
+        self.bottomRight = bottomRight
+
 def xCoordinates(sobel_img):
     num_rows = float(len(sobel_img)) # get number of x values
 
@@ -15,7 +21,7 @@ def xCoordinates(sobel_img):
     xnew = np.arange(0,len(vert_sum),50) # for smoothing
     #smooth
     y_smooth = spline(x, vert_sum, xnew)
-    
+
     #make a sin curve 300px wide
     z = np.arange(0,300,1)
     def f(x):
@@ -28,23 +34,7 @@ def xCoordinates(sobel_img):
 
     # detect local minima
     mins = (np.diff(np.sign(np.diff(y_conv))) > 0).nonzero()[0] + 1
-    
 
-    # graph
-    #plt.title("Tree candidates for "+imgpath)
-    #plt.xlabel("X Coordinate of pixels in image")
-    #plt.ylabel("Average grayscale convolved with sine curve")
-
-    # plt.plot(x,vert_sum,'g-',x,y_conv,'r-')
-    #plt.plot(x,y_conv,'b-',mins,[y_conv[x] for x in mins],'o')
-
-    # plot lines over image where minima are
-    #img = cv2.imread(imgpath)
-    #for i in mins:
-    #    cv2.line(img,(i,0),(i,int(num_rows)),(255, 0, 0),3)
-
-    #cv2.imwrite('lines.png',img)
-    #plt.show()
     return mins
 
 def yCoordinates(sobel_img):
@@ -55,9 +45,9 @@ def yCoordinates(sobel_img):
     horiz_sum = np.divide(horiz_sum, num_col)
 
     y = np.arange(0, len(horiz_sum))
-    ynew = np.arange(0, len(horiz_sum)) 
+    ynew = np.arange(0, len(horiz_sum))
     x_smooth = spline(y, horiz_sum, ynew)
-    
+
     #make a sin curve 300px wide
     z = np.arange(0,300,1)
     def f(x):
@@ -70,95 +60,95 @@ def yCoordinates(sobel_img):
 
     # detect local minima
     mins = (np.diff(np.sign(np.diff(y_conv))) > 0).nonzero()[0] + 1
-    
-    #plt.title("Tree candidates for "+imgpath)
-    #plt.xlabel("Y Coordinate of pixels in image")
-    #plt.ylabel("Average grayscale convolved with sine curve")
 
-    # plt.plot(x,vert_sum,'g-',x,y_conv,'r-')
-    #plt.plot(y,y_conv,'b-',mins,[y_conv[x] for x in mins],'o')
-
-    # plot lines over image where minima are
-    #img = cv2.imread(imgpath)
-    #for i in mins:
-    #    cv2.line(img,(0, i),(int(num_col), i),(255, 0, 0),3)
-
-    #cv2.imwrite('lines.png',img)
-
-    #plt.show()
     return mins
 
+
 """
-Sobel
+Sobel reference
 http://www.jayrambhia.com/blog/sobel-operator/
 """
-imgpath = 'tree2.jpg' # which image to do
+
+def find_trees(imgpath):
+    # get image, reshape
+    img1 = cv2.imread(imgpath,0)
+    Z = img1.reshape((-1,3))
+    # convert to np.float32
+    Z = np.float32(Z)
+
+    # define criteria, number of clusters(K) and apply kmeans()
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
+    K = 2
+    ret,label,center=cv2.kmeans(Z,K,criteria,10,cv2.KMEANS_RANDOM_CENTERS)
+    center = np.uint8(center)
+    res = center[label.flatten()]
+    res2 = res.reshape((img1.shape))
+    gray = img1
+    ret, thresh = cv2.threshold(gray,0,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
 
 
-# get image, reshape
-img1 = cv2.imread(imgpath,0)
-Z = img1.reshape((-1,3))
-# convert to np.float32
-Z = np.float32(Z)
+    # apply blur filter
+    img = cv2.blur(res2,(1,1))
 
-# define criteria, number of clusters(K) and apply kmeans()
-criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
-K = 2
-ret,label,center=cv2.kmeans(Z,K,criteria,10,cv2.KMEANS_RANDOM_CENTERS)
-center = np.uint8(center)
-res = center[label.flatten()]
-res2 = res.reshape((img1.shape))
-gray = img1
-ret, thresh = cv2.threshold(gray,0,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
+    # apply Sobel operator
+    dst = cv2.Sobel(img, ddepth=cv2.cv.CV_8U, dx=1, dy=0, ksize=5)
+    minv = np.min(dst)
+    maxv = np.max(dst)
+    cscale = 255/(maxv-minv)
+    shift =  -1*(minv)
+    sobel_img = np.zeros(img.shape,dtype='uint8')
+    sobel_img = cv2.convertScaleAbs(dst, sobel_img, cscale, shift/255.0)
 
+    #Take the coordinates of the x and y values from the min values
+    xMin = xCoordinates(sobel_img)
+    yMin = yCoordinates(sobel_img)
 
-# apply blur filter
-img = cv2.blur(res2,(1,1))
+    #Take the middle 20 values of the coordinates for the tree
+    xIndex = int(len(xMin)/2) - 11
+    yIndex = int(len(yMin)/2) - 11
 
-# apply Sobel operator
-dst = cv2.Sobel(img, ddepth=cv2.cv.CV_8U, dx=1, dy=0, ksize=5)
-minv = np.min(dst)
-maxv = np.max(dst)
-cscale = 255/(maxv-minv)
-shift =  -1*(minv)
-sobel_img = np.zeros(img.shape,dtype='uint8')
-sobel_img = cv2.convertScaleAbs(dst, sobel_img, cscale, shift/255.0)
+    xMaxIndex = xIndex + 20
+    yMaxIndex = yIndex + 20
 
-#Take the coordinates of the x and y values from the min values
-xMin = xCoordinates(sobel_img)
-yMin = yCoordinates(sobel_img)
+    #Put the (x,y) coordinates for the tree into an array
+    treeLocator = []
+    t = 0
 
-#Take the middle 20 values of the coordinates for the tree
-xIndex = int(len(xMin)/2) - 11
-yIndex = int(len(yMin)/2) - 11
+    while(xIndex <= xMaxIndex):
+        treeLocator.append([])
+        treeLocator[t].append(xMin[xIndex])
+        treeLocator[t].append(yMin[yIndex])
+        xIndex += 1
+        yIndex += 1
+        t += 1
+    #Draw the lines for the four corners of the tree using the min and max coordinates from the array
+    img = cv2.imread(imgpath)
 
-xMaxIndex = xIndex + 20
-yMaxIndex = yIndex + 20
+    # return coordinates of top left and top right points
+    return (treeLocator[0][0],treeLocator[0][1]), (treeLocator[20][0],treeLocator[20][1])
 
-#Put the (x,y) coordinates for the tree into an array
-treeLocator = []
-t = 0
-while(xIndex <= xMaxIndex):
-    treeLocator.append([])
-    treeLocator[t].append(xMin[xIndex])
-    treeLocator[t].append(yMin[yIndex])
-    xIndex += 1
-    yIndex += 1
-    t += 1
+def draw_boxes(imgpath, tree):
+    x1, y1 = tree.topLeft
+    x2, y2 = tree.bottomRight
 
-#Draw the lines for the four corners of the tree using the min and max coordinates from the array
-img = cv2.imread(imgpath)
-cv2.line(img,(treeLocator[0][0],treeLocator[0][1]),(treeLocator[20][0],treeLocator[0][1]),(255, 0, 0),3)
-cv2.line(img,(treeLocator[0][0],treeLocator[0][1]),(treeLocator[0][0],treeLocator[20][1]),(255, 0, 0),3)
-cv2.line(img,(treeLocator[0][0],treeLocator[20][1]),(treeLocator[20][0],treeLocator[20][1]),(255, 0, 0),3)
-cv2.line(img,(treeLocator[20][0],treeLocator[0][1]),(treeLocator[20][0],treeLocator[20][1]),(255, 0, 0),3)
+    img = cv2.imread(imgpath)
 
-cv2.imwrite('lines.png',img)
+    cv2.line(img,(x1,y1),(x2,y1),(255, 0, 0),3)
+    cv2.line(img,(x1,y1),(x1,y2),(255, 0, 0),3)
+    cv2.line(img,(x1,y2),(x2,y2),(255, 0, 0),3)
+    cv2.line(img,(x2,y1),(x2,y2),(255, 0, 0),3)
 
+    imgpath = imgpath.split('/')[1]
+    cv2.imwrite('boxes_'+imgpath,img)
 
+if __name__ == '__main__':
 
-# cv2.imwrite('sobel.png',sobel_img)
+    img1, img2 = 'data/tree2.jpg', 'data/tree3.jpg'
 
-# laplacian = cv2.Laplacian(sobel_img,cv2.CV_64F)
-# cv2.imwrite('laplacian.png',laplacian)
+    topLeft, bottomRight = find_trees(img1)
+    tree1 = Tree(img1, topLeft, bottomRight)
 
+    topLeft, bottomRight = find_trees(img2)
+    tree2 = Tree(img2, topLeft, bottomRight)
+
+    draw_boxes(img1, tree1)
--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-
-
+import final
 import numpy as np
 import cv2
 from matplotlib import pyplot as plt
@@ -64,67 +63,6 @@ def drawMatches(img1, kp1, img2, kp2, matches):
     return out
 
 
-"""
-Kmeans clustering
-"""
-# Z = img1.reshape((-1,3))
-# # convert to np.float32
-# Z = np.float32(Z)
-#
-# # define criteria, number of clusters(K) and apply kmeans()
-# criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
-# K = 2
-# ret,label,center=cv2.kmeans(Z,K,criteria,10,cv2.KMEANS_RANDOM_CENTERS)
-# center = np.uint8(center)
-# res = center[label.flatten()]
-# res2 = res.reshape((img1.shape))
-# gray = img1
-# ret, thresh = cv2.threshold(gray,0,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
-# # noise removal
-# kernel = np.ones((3,3),np.uint8)
-# opening = cv2.morphologyEx(thresh,cv2.MORPH_OPEN,kernel, iterations = 3)
-#
-# cv2.imwrite('clustered.png',res2)
-
-
-"""
-Image Segmentation
-"""
-# gray = cv2.imread('canny.png',0)
-# ret, thresh = cv2.threshold(gray,0,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
-#
-# # noise removal
-# kernel = np.ones((3,3),np.uint8)
-# opening = cv2.morphologyEx(thresh,cv2.MORPH_OPEN,kernel, iterations = 3)
-#
-# # sure background area
-# sure_bg = cv2.dilate(opening,kernel,iterations=3)
-#
-# # Finding sure foreground area
-# dist_transform = cv2.distanceTransform(opening,cv2.cv.CV_DIST_L2,5)
-# ret, sure_fg = cv2.threshold(dist_transform,0.7*dist_transform.max(),255,0)
-#
-# # Finding unknown region
-# sure_fg = np.uint8(sure_fg)
-# unknown = cv2.subtract(sure_bg,sure_fg)
-#
-# cv2.imwrite('sure_bg.png',sure_bg)
-# cv2.imwrite('sure_fg.png',sure_fg)
-
-
-
-# """
-# find contours
-# """
-#
-# im = cv2.imread('canny.png')
-# imgray = cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
-# ret,thresh = cv2.threshold(imgray,127,255,0)
-# contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-# cv2.drawContours(im,contours,-1,(0,255,0),3)
-# cv2.imwrite("contours.png", im)
-
-
 #
 # """
 # Hough Line Transformation
@@ -146,7 +84,7 @@ Image Segmentation
 SIFT feature detection
 """
 
-#
+# 
 # # Initiate SIFT detector
 # orb = cv2.ORB()
 #
@@ -168,36 +106,16 @@ SIFT feature detection
 # cv2.imwrite('matches.png',img3)
 # # plt.imshow(img3),plt.show()
 
-
-"""
-train classifier
-"""
-
-# color = [0, 255, 0]
-# thickness   = 2
-# cascadeFile = './opencv-haar-classifier-training/trained_classifiers/tree_classifier.xml'
-#
-# for i in range(11):
-#     fileName = './opencv-haar-classifier-training/positive_images/pos'+str(i+1)+'.png'
-#     im = cv2.imread(fileName)
-#     trees = cv2.CascadeClassifier(cascadeFile)
-#     objects = trees.detectMultiScale(im, 1.3, 5)
-#     for (x, y, w, h) in objects:
-#         im = cv2.rectangle(im,(x,y),(x+w,y+h),(255,0,0),2)
-#     cv2.imshow('im',im)
-#     cv2.waitKey(0)
-#     cv2.destroyAllWindows()
-
-
 """
 Sobel
 http://www.jayrambhia.com/blog/sobel-operator/
 """
-imgpath = 'tree2.jpg' # which image to do
+imgpath = "data/tree3.jpg" # which image to do
 
-
-# get image, reshape
+# get image
 img1 = cv2.imread(imgpath,0)
+
+# reshape
 Z = img1.reshape((-1,3))
 # convert to np.float32
 Z = np.float32(Z)
@@ -209,9 +127,8 @@ ret,label,center=cv2.kmeans(Z,K,criteria,10,cv2.KMEANS_RANDOM_CENTERS)
 center = np.uint8(center)
 res = center[label.flatten()]
 res2 = res.reshape((img1.shape))
-gray = img1
-ret, thresh = cv2.threshold(gray,0,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
-
+# gray = img1
+# ret, thresh = cv2.threshold(gray,0,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
 
 # apply blur filter
 img = cv2.blur(res2,(1,1))
@@ -226,43 +143,49 @@ sobel_img = np.zeros(img.shape,dtype='uint8')
 sobel_img = cv2.convertScaleAbs(dst, sobel_img, cscale, shift/255.0)
 
 
-num_rows = float(len(sobel_img)) # get number of x values
-# sum along y axis
-vert_sum = np.sum(sobel_img,axis=0)
-# make it an average value (divide by # of x values)
-vert_sum = np.divide(vert_sum,num_rows)
+# num_rows = float(len(sobel_img)) # get number of x values
+# # sum along y axis
+# vert_sum = np.sum(sobel_img,axis=0)
+# # make it an average value (divide by # of x values)
+# vert_sum = np.divide(vert_sum,num_rows)
+#
+#
+# x = np.arange(0,len(vert_sum)) # for graphing
+# xnew = np.arange(0,len(vert_sum),50) # for smoothing
+# #smooth
+# y_smooth = spline(x, vert_sum, xnew)
+#
+# #make a sin curve 300px wide
+# z = np.arange(0,300,1)
+# def f(x):
+#     return np.sin(x/90)*-10 + 25
+#
+# f = [f(i) for i in z] # make sine into an array
+#
+# # convolve sine and the vertical sum
+# y_conv = np.convolve(vert_sum, f,'same')
+# y_conv = np.convolve(y_conv, f,'same')
+#
+# # detect local minima, which are tree candidates
+# mins = (np.diff(np.sign(np.diff(y_conv))) > 0).nonzero()[0] + 1
 
+x_mins = final.xCoordinates(sobel_img)
+y_mins = final.yCoordinates(sobel_img)
 
-x = np.arange(0,len(vert_sum)) # for graphing
-xnew = np.arange(0,len(vert_sum),50) # for smoothing
-#smooth
-y_smooth = spline(x, vert_sum, xnew)
+# process tree candidates
 
-#make a sin curve 300px wide
-z = np.arange(0,300,1)
-def f(x):
-    return np.sin(x/90)*-15 + 25
-
-f = [f(i) for i in z] # make sine into an array
-
-# convolve sine and the vertical sum
-y_conv = np.convolve(vert_sum, f,'same')
-
-# detect local minima
-mins = (np.diff(np.sign(np.diff(y_conv))) > 0).nonzero()[0] + 1
-
-# graph
-plt.title("Tree candidates for "+imgpath)
-plt.xlabel("X Coordinate of pixels in image")
-plt.ylabel("Average grayscale convolved with sine curve")
-
-# plt.plot(x,vert_sum,'g-',x,y_conv,'r-')
-plt.plot(x,y_conv,'b-',mins,[y_conv[x] for x in mins],'o')
+# # graph
+# plt.title("Tree candidates for "+imgpath)
+# plt.xlabel("X Coordinate of pixels in image")
+# plt.ylabel("Average grayscale convolved with sine curve")
+#
+# # plt.plot(x,vert_sum,'g-',x,y_conv,'r-')
+# plt.plot(x,y_conv,'b-',mins,[y_conv[x] for x in mins],'o')
 
 # plot lines over image where minima are
 img = cv2.imread(imgpath)
-for i in mins:
-    cv2.line(img,(i,0),(i,int(num_rows)),(255, 0, 0),3)
+for i in x_mins:
+    cv2.line(img,(i,0),(i,len(sobel_img)),(255, 0, 0),2)
 
 cv2.imwrite('lines.png',img)
 
